ed6902e6501cea9eb128e2000aa95c0bee2cde44
jihyde <jonahihyde135@gmail.com>
2021-08-19 16:58:20 +0100

    testcomponent 2 changes and version increment

--- build/TestComponent2/TestComponent2.d.ts
+++ build/TestComponent2/TestComponent2.d.ts
@@ -1,5 +1,5 @@
 import React from "react";
 import { TestComponent2Props } from "./TestComponent2.types";
-import "./TestComponent2scss";
+import "./TestComponent2.scss";
 declare const TestComponent2: React.FC<TestComponent2Props>;
 export default TestComponent2;
